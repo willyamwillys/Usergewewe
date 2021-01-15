61526064b9bdb7b614959a69557068918a81a36b
willyamwillys <76863131+willyamwillys@users.noreply.github.com>
2021-01-15 10:49:20 +0800

    Update pmpermit.py

--- userge/plugins/utils/pmpermit.py
+++ userge/plugins/utils/pmpermit.py
@@ -24,40 +24,40 @@ pmCounter: Dict[int, int] = {}
 _IS_INLINE = True
 allowAllFilter = filters.create(lambda _, __, ___: Config.ALLOW_ALL_PMS)
 noPmMessage = bk_noPmMessage = ("**-ID**\n"
-                                "Hai Ngab 😁 {flname} ({uname}). Ini adalah pesan otomatis gua.\n"
-                                "Tunggu sampai lu di Approve atau gua balas.\n "
-                                "Jadi jangan **SPAM** gua ngab.\n"
+                                "Halo 😁 {flname} ({uname}). ini adalah pesan otomatis \ n "
+                                "Harap tunggu sampai Anda disetujui untuk mengirim pesan langsung \ n"
+                                "Dan tolong jangan ** SPAM ** sampai saat itu\n"
                                "\n"
                                 "**-EN**\n"
                                "Hello 😁 {flname} ({uname}). this is an automated message\n"
                                "Please wait until you get approved to direct message\n "
                                "And please dont **SPAM** until then\n ")
-blocked_message = bk_blocked_message = "**-ID**\n**Lu otomatis ke blok**\n\n**-EN**\n**You were automatically blocked**"
+blocked_message = bk_blocked_message = "**-ID**\nAnda diblokir secara otomatis\n**-EN**\nYou were automatically blocked"
 
 
 async def _init() -> None:
     global noPmMessage, blocked_message, _IS_INLINE  # pylint: disable=global-statement
     async for chat in ALLOWED_COLLECTION.find({"status": 'allowed'}):
         Config.ALLOWED_CHATS.add(chat.get("_id"))
-    _pm = await SAVED_SETTINGS.find_one({'_id': 'PM GUARD STATUS'})
+    _pm = await SAVED_SETTINGS.find_one({'_id': 'STATUS PENJAGA PM'})
     if _pm:
         Config.ALLOW_ALL_PMS = bool(_pm.get('data'))
     i_pm = await SAVED_SETTINGS.find_one({'_id': 'INLINE_PM_PERMIT'})
     if i_pm:
         _IS_INLINE = bool(i_pm.get('data'))
-    _pmMsg = await SAVED_SETTINGS.find_one({'_id': 'CUSTOM NOPM PESAN'})
+    _pmMsg = await SAVED_SETTINGS.find_one({'_id': 'PESAN CUSTOM NOPM'})
     if _pmMsg:
         noPmMessage = _pmMsg.get('data')
-    _blockPmMsg = await SAVED_SETTINGS.find_one({'_id': 'CUSTOM PESAN BLOKIR'})
+    _blockPmMsg = await SAVED_SETTINGS.find_one({'_id': 'PESAN CUSTOM BLOCKPM'})
     if _blockPmMsg:
         blocked_message = _blockPmMsg.get('data')
 
 
 @userge.on_cmd("allow", about={
-    'header': "allows someone to contact",
-    'description': "Ones someone is allowed, "
-                   "Userge will not interfere or handle such private chats",
-    'usage': "{tr}allow [username | userID]\nreply {tr}allow to a message, "
+    'header': "memungkinkan seseorang untuk menghubungi",
+    'description': "Seseorang diizinkan, "
+                   "Userge tidak akan mengganggu atau menangani obrolan pribadi semacam itu",
+    'usage': "{tr}allow [username | userID]\nreply {tr}allow ke sebuah pesan, "
              "do {tr}allow in the private chat"}, allow_channels=False, allow_via_bot=False)
 async def allow(message: Message):
     """ allows to pm """
@@ -69,21 +69,21 @@ async def allow(message: Message):
         a = await ALLOWED_COLLECTION.update_one(
             {'_id': userid}, {"$set": {'status': 'allowed'}}, upsert=True)
         if a.matched_count:
-            await message.edit("`telah di izinkan mengirim pesan`", del_in=3)
+            await message.edit("`Sudah disetujui untuk mengirim pesan langsung`", del_in=3)
         else:
             await (await userge.get_users(userid)).unblock()
-            await message.edit("`di izinkan mengirim pesan`", del_in=3)
+            await message.edit("`Disetujui untuk mengirim pesan langsung`", del_in=3)
     else:
         await message.edit(
-            "gua perlu lu chat orang di private chat",
+            "Saya perlu membalas ke pengguna atau memberikan nama pengguna / id atau berada dalam obrolan pribadi",
             del_in=3)
 
 
 @userge.on_cmd("nopm", about={
-    'header': "Activates guarding on inbox",
-    'description': "Ones someone is allowed, "
-                   "Userge will not interfere or handle such private chats",
-    'usage': "{tr}nopm [username | userID]\nreply {tr}nopm to a message, "
+    'header': "Mengaktifkan penjagaan di kotak masuk",
+    'description': "Seseorang diizinkan, "
+                   "Userge tidak akan mengganggu atau menangani obrolan pribadi semacam itu",
+    'usage': "{tr}nopm [username | userID]\nbalasan {tr}nopm ke sebuah pesan, "
              "do {tr}nopm in the private chat"}, allow_channels=False, allow_via_bot=False)
 async def denyToPm(message: Message):
     """ disallows to pm """
@@ -93,12 +93,12 @@ async def denyToPm(message: Message):
             Config.ALLOWED_CHATS.remove(userid)
         a = await ALLOWED_COLLECTION.delete_one({'_id': userid})
         if a.deleted_count:
-            await message.edit("`dilarang mengirim pesan`", del_in=3)
+            await message.edit("`Dilarang mengirim pesan langsung`", del_in=3)
         else:
-            await message.edit("`ga ada yg berubah`", del_in=3)
+            await message.edit("`Tidak ada yang berubah`", del_in=3)
     else:
         await message.edit(
-            "gua perlu lu chat orang di private chat",
+            "Saya perlu membalas ke pengguna atau memberikan nama pengguna / id atau berada dalam obrolan pribadi",
             del_in=3)
 
 
@@ -119,21 +119,21 @@ async def get_id(message: Message):
 
 @userge.on_cmd(
     "pmguard", about={
-        'header': "Switchs the pm permiting module on",
-        'description': "This is switched off in default. "
-                       "You can switch pmguard On or Off with this command. "
-                       "When you turn on this next time, "
-                       "the previously allowed chats will be there !"},
+        'header': "Mengaktifkan modul izin pm",
+        'description': "Ini dimatikan secara default. "
+                       "Anda dapat mengaktifkan atau menonaktifkan pmguard dengan perintah ini. "
+                       "Saat Anda mengaktifkannya lain kali, "
+                       "obrolan yang sebelumnya diizinkan akan ada di sana !"},
     allow_channels=False)
 async def pmguard(message: Message):
     """ enable or disable auto pm handler """
     global pmCounter  # pylint: disable=global-statement
     if Config.ALLOW_ALL_PMS:
         Config.ALLOW_ALL_PMS = False
-        await message.edit("`PM_guard activated`", del_in=3, log=__name__)
+        await message.edit("`PM_guard diaktifkan`", del_in=3, log=__name__)
     else:
         Config.ALLOW_ALL_PMS = True
-        await message.edit("`PM_guard deactivated`", del_in=3, log=__name__)
+        await message.edit("`PM_guard dinonaktifkan`", del_in=3, log=__name__)
         pmCounter.clear()
     await SAVED_SETTINGS.update_one(
         {'_id': 'PM GUARD STATUS'}, {"$set": {'data': Config.ALLOW_ALL_PMS}}, upsert=True)
@@ -141,8 +141,8 @@ async def pmguard(message: Message):
 
 @userge.on_cmd(
     "ipmguard", about={
-        'header': "Switchs the Inline pm permiting module on",
-        'description': "This is switched off in default.",
+        'header': "Mengaktifkan modul izin pm Inline",
+        'description': "Ini dimatikan secara default.",
         'usage': "{tr}ipmguard"},
     allow_channels=False)
 async def ipmguard(message: Message):
@@ -150,39 +150,39 @@ async def ipmguard(message: Message):
     global _IS_INLINE  # pylint: disable=global-statement
     if _IS_INLINE:
         _IS_INLINE = False
-        await message.edit("`Inline PM_guard activated`", del_in=3, log=__name__)
+        await message.edit("`Inline PM_guard dinonaktifkan`", del_in=3, log=__name__)
     else:
         _IS_INLINE = True
-        await message.edit("`Inline PM_guard deactivated`", del_in=3, log=__name__)
+        await message.edit("`Inline PM_guard diaktifkan`", del_in=3, log=__name__)
     await SAVED_SETTINGS.update_one(
         {'_id': 'INLINE_PM_PERMIT'}, {"$set": {'data': _IS_INLINE}}, upsert=True)
 
 
 @userge.on_cmd("setpmmsg", about={
-    'header': "Sets the reply message",
-    'description': "You can change the default message which userge gives on un-invited PMs",
-    'flags': {'-r': "reset to default"},
+    'header': "Mengatur pesan balasan",
+    'description': "Anda dapat mengubah pesan default yang diberikan userge pada PM yang tidak diundang",
+    'flags': {'-r': "reset ke default"},
     'options': {
-        '{fname}': "add first name",
-        '{lname}': "add last name",
-        '{flname}': "add full name",
-        '{uname}': "username",
-        '{chat}': "chat name",
-        '{mention}': "mention user"}}, allow_channels=False)
+        '{fname}': "tambahkan nama depan",
+        '{lname}': "tambahkan nama belakang",
+        '{flname}': "tambahkan nama lengkap",
+        '{uname}': "nama pengguna",
+        '{chat}': "nama obrolan",
+        '{mention}': "sebutkan pengguna"}}, allow_channels=False)
 async def set_custom_nopm_message(message: Message):
     """ setup custom pm message """
     global noPmMessage  # pylint: disable=global-statement
     if '-r' in message.flags:
-        await message.edit('`Custom NOpm message reset`', del_in=3, log=True)
+        await message.edit('`Atur ulang pesan NOpm kustom`', del_in=3, log=True)
         noPmMessage = bk_noPmMessage
-        await SAVED_SETTINGS.find_one_and_delete({'_id': 'CUSTOM NOPM MESSAGE'})
+        await SAVED_SETTINGS.find_one_and_delete({'_id': 'PESAN CUSTOM NOPM'})
     else:
         string = message.input_or_reply_raw
         if string:
-            await message.edit('`Custom NOpm message saved`', del_in=3, log=True)
+            await message.edit('`Pesan NOpm kustom disimpan`', del_in=3, log=True)
             noPmMessage = string
             await SAVED_SETTINGS.update_one(
-                {'_id': 'CUSTOM NOPM MESSAGE'}, {"$set": {'data': string}}, upsert=True)
+                {'_id': 'PESAN CUSTOM NOPM'}, {"$set": {'data': string}}, upsert=True)
         else:
             await message.err("invalid input!")
 
@@ -202,31 +202,31 @@ async def change_inline_message(message: Message):
 
 
 @userge.on_cmd("setbpmmsg", about={
-    'header': "Sets the block message",
-    'description': "You can change the default blockPm message "
-                   "which userge gives on un-invited PMs",
-    'flags': {'-r': "reset to default"},
+    'header': "Mengatur pesan blok",
+    'description': "Anda dapat mengubah pesan default blockPm"
+                    "yang diberikan pengguna pada PM yang tidak diundang",
+    'flags': {'-r': "reset ke default"},
     'options': {
-        '{fname}': "add first name",
-        '{lname}': "add last name",
-        '{flname}': "add full name",
-        '{uname}': "username",
-        '{chat}': "chat name",
-        '{mention}': "mention user"}}, allow_channels=False)
+        '{fname}': "tambahkan nama depan",
+        '{lname}': "tambahkan nama belakang",
+        '{flname}': "tambahkan nama lengkap",
+        '{uname}': "nama pengguna",
+        '{chat}': "nama obrolan",
+        '{mention}': "sebutkan pengguna"}}, allow_channels=False)
 async def set_custom_blockpm_message(message: Message):
     """ setup custom blockpm message """
     global blocked_message  # pylint: disable=global-statement
     if '-r' in message.flags:
         await message.edit('`Custom BLOCKpm message reset`', del_in=3, log=True)
         blocked_message = bk_blocked_message
-        await SAVED_SETTINGS.find_one_and_delete({'_id': 'CUSTOM BLOCKPM MESSAGE'})
+        await SAVED_SETTINGS.find_one_and_delete({'_id': 'PESAN CUSTOM BLOCKPM'})
     else:
         string = message.input_or_reply_raw
         if string:
-            await message.edit('`Custom BLOCKpm message saved`', del_in=3, log=True)
+            await message.edit('`Pesan BLOCKpm kustom disimpan`', del_in=3, log=True)
             blocked_message = string
             await SAVED_SETTINGS.update_one(
-                {'_id': 'CUSTOM BLOCKPM MESSAGE'}, {"$set": {'data': string}}, upsert=True)
+                {'_id': 'PESAN CUSTOM BLOCKPM'}, {"$set": {'data': string}}, upsert=True)
         else:
             await message.err("invalid input!")
 
@@ -237,7 +237,7 @@ async def set_custom_blockpm_message(message: Message):
     allow_channels=False)
 async def view_current_noPM_msg(message: Message):
     """ view current pm message """
-    await message.edit(f"--pesan PM saat ini--\n\n{noPmMessage}")
+    await message.edit(f"--current PM message--\n\n{noPmMessage}")
 
 
 @userge.on_cmd(
@@ -246,7 +246,7 @@ async def view_current_noPM_msg(message: Message):
     allow_channels=False)
 async def view_current_blockPM_msg(message: Message):
     """ view current block pm message """
-    await message.edit(f"--pesan PM Blokir saat ini--\n\n{blocked_message}")
+    await message.edit(f"--current blockPM message--\n\n{blocked_message}")
 
 
 @userge.on_filters(~allowAllFilter & filters.incoming & filters.private & ~filters.bot
@@ -266,12 +266,12 @@ async def uninvitedPmHandler(message: Message):
             await message.from_user.block()
             await asyncio.sleep(1)
             await CHANNEL.log(
-                f"#BLOCKED\n{user_dict['mention']} telah di blok karna spam di pm !! ")
+                f"#BLOCKED\n{user_dict['mention']} has been blocked due to spamming in pm !! ")
         else:
             pmCounter[message.from_user.id] += 1
             await message.reply(
-                f"lu ada {pmCounter[message.from_user.id]} dari 4 **Peringatan**\n"
-                "tunggu sampe lu di izinkan ngab !", del_in=10)
+                f"Kamu punya {pmCounter[message.from_user.id]} dari 4 ** Peringatan**\n"
+                "Harap tunggu sampai Anda mendapatkan persetujuan untuk pm !", del_in=5)
     else:
         pmCounter.update({message.from_user.id: 1})
         if userge.has_bot and _IS_INLINE:
@@ -289,7 +289,7 @@ async def uninvitedPmHandler(message: Message):
             await message.reply(
                 noPmMessage.format_map(SafeDict(**user_dict)) + '\n`- Protected by WillyamWillys`')
         await asyncio.sleep(1)
-        await CHANNEL.log(f"#Pesan_Baru\n{user_dict['mention']} telah mengirim lu pesan")
+        await CHANNEL.log(f"#NEW_MESSAGE\n{user_dict['mention']} has messaged you")
 
 
 @userge.on_filters(~allowAllFilter & filters.outgoing & ~filters.edited
@@ -303,7 +303,7 @@ async def outgoing_auto_approve(message: Message):
     await ALLOWED_COLLECTION.update_one(
         {'_id': userID}, {"$set": {'status': 'allowed'}}, upsert=True)
     user_dict = await userge.get_user_dict(userID)
-    await CHANNEL.log(f"**#Izinkan_Otomatis**\n{user_dict['mention']}")
+    await CHANNEL.log(f"**#AUTO_APPROVED**\n{user_dict['mention']}")
 
 if userge.has_bot:
     @userge.bot.on_callback_query(filters.regex(pattern=r"pm_allow\((.+?)\)"))
@@ -315,27 +315,28 @@ if userge.has_bot:
             user = await userge.get_users(userID)
             if userID in Config.ALLOWED_CHATS:
                 await c_q.edit_message_text(
-                    f"{user.mention} sudah di izinkan mengirim pesan.")
+                    f"{user.mention} sudah diizinkan untuk Direct Message.")
             else:
                 await c_q.edit_message_text(
-                    f"{user.mention} `di izinkan mengirim pesan.`")
+                    f"{user.mention} allowed to Direct Messages.")
                 await userge.send_message(
-                    userID, f"{owner.mention} `lu di izinkan mengirim pesan.`")
+                    userID, f"{owner.mention} `menyetujui Anda untuk Direct Message.`")
                 if userID in pmCounter:
                     del pmCounter[userID]
                 Config.ALLOWED_CHATS.add(userID)
                 await ALLOWED_COLLECTION.update_one(
                     {'_id': userID}, {"$set": {'status': 'allowed'}}, upsert=True)
         else:
-            await c_q.answer(f"cuma {owner.first_name} dapat akses Izinkan.")
+            await c_q.answer(f"Hanya {owner.first_name} memiliki akses ke Izinkan.")
 
     @userge.bot.on_callback_query(filters.regex(pattern=r"pm_block\((.+?)\)"))
     async def pm_callback_block(_, c_q: CallbackQuery):
         owner = await userge.get_me()
         if c_q.from_user.id == owner.id:
             userID = int(c_q.matches[0].group(1))
+            user_dict = await userge.get_user_dict(userID)
             await userge.send_message(
-                userID, f"{owner.mention} `sudah diblokir, maaf ngab.`")
+                userID, blocked_message.format_map(SafeDict(**user_dict)))
             await userge.block_user(userID)
             if userID in pmCounter:
                 del pmCounter[userID]
@@ -345,18 +346,18 @@ if userge.has_bot:
             user = await userge.get_users(userID)
             if k.deleted_count:
                 await c_q.edit_message_text(
-                    f"{user.mention} `dilarang mengirim pesan langsung`")
+                    f"{user.mention} `Dilarang mengirim pesan langsung`")
             else:
                 await c_q.edit_message_text(
-                    f"{user.mention} `sudah dilarang mengirim pesan langsung.`")
+                    f"{user.mention} `sudah Dilarang mengirim pesan langsung.`")
         else:
-            await c_q.answer(f"cuma {owner.first_name} bisa akses blokir.")
+            await c_q.answer(f"Hanya {owner.first_name} memiliki akses ke Blokir.")
 
     @userge.bot.on_callback_query(filters.regex(pattern=r"^pm_spam$"))
     async def pm_spam_callback(_, c_q: CallbackQuery):
         owner = await userge.get_me()
         if c_q.from_user.id == owner.id:
-            await c_q.answer("maaf, lu ga bisa klik sendiri ngab")
+            await c_q.answer("Maaf, Anda tidak dapat mengklik sendiri")
         else:
             del pmCounter[c_q.from_user.id]
             user_dict = await userge.get_user_dict(c_q.from_user.id)
@@ -365,13 +366,13 @@ if userge.has_bot:
             await userge.block_user(c_q.from_user.id)
             await asyncio.sleep(1)
             await CHANNEL.log(
-                f"#BLOCKED\n{c_q.from_user.mention} telah di blok karna spam di pm !! ")
+                f"#BLOCKED\n{c_q.from_user.mention} telah diblokir karena spamming di pm !! ")
 
     @userge.bot.on_callback_query(filters.regex(pattern=r"^pm_contact$"))
     async def pm_contact_callback(_, c_q: CallbackQuery):
         owner = await userge.get_me()
         if c_q.from_user.id == owner.id:
-            await c_q.answer("maaf, lu ga bisa klik sendiri ngab")
+            await c_q.answer("Maaf, Anda tidak dapat mengklik sendiri")
         else:
             user_dict = await userge.get_user_dict(c_q.from_user.id)
             await c_q.edit_message_text(
@@ -380,14 +381,14 @@ if userge.has_bot:
                 [
                     [
                         InlineKeyboardButton(
-                            text="Terima", callback_data=f"pm_allow({c_q.from_user.id})"),
+                            text="Mengizinkan", callback_data=f"pm_allow({c_q.from_user.id})"),
                         InlineKeyboardButton(
-                            text="Blokir", callback_data=f"pm_block({c_q.from_user.id})")
+                            text="Memblokir", callback_data=f"pm_block({c_q.from_user.id})")
                     ]
                 ]
             )
             await userge.bot.send_message(
                 owner.id,
-                f"{c_q.from_user.mention} mau nge pm lu.",
+                f"{c_q.from_user.mention} PESAN CUSTOM NOPM.",
                 reply_markup=buttons
             )
